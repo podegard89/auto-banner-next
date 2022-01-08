e9bd5811a8a42e3231e214d743ccfed4004c0d81
Pearce Odegard <pearceodegard@gmail.com>
2022-01-08 01:50:52 -0700

    Beginning reorganization of state

---
+++ components/ClockInClockOut.js
@@ -0,0 +1,33 @@
+import LoadingButton from '@mui/lab/LoadingButton'
+import { useState } from 'react'
+
+const ClockInClockOut = (props) => {
+    const clockIn = async () => {
+        setLoading(true);
+        const json = await httpPost('api/clock-in', date);
+        console.log(json)
+        setStart(json.start);
+        setClockedStatus(json.status);
+        setIsClockedIn(true);
+        setLoading(false);
+    }
+
+    const clockOut = async () => {
+        setLoading(true);
+        const json = await httpPost('api/clock-out', date);
+        setEnd(json.end);
+        setClockedStatus(json.status);
+        setHours(json.hours);
+        setIsClockedIn(false);
+        setIsDoneForDay(true);
+        setLoading(false);
+    }
+
+    return (
+        <LoadingButton variant="contained" onClick={clockIn} loading={loading}>
+            Clock in ⏰
+        </LoadingButton>
+    )
+}
+
+export default ClockInButton;
